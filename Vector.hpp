5831ca2f9acd34b0f68d58a7004d89c35b7df271
Othmane Bounri <obounri@e2r10p5.1337.ma>
2022-05-12 19:30:32 +0100

    iterators, reverse_iterators, r/begin(), r/end(), to test

--- Vector.hpp
+++ Vector.hpp
@@ -5,44 +5,9 @@
 #include <exception>
 #include <iostream>
 #include <memory>
-#include <iterator>
+// #include <iterator>
 
-// class IndexOutOfRange: public std::exception {
-// private:
-//     const char * exception;
-// public:
-//     IndexOutOfRange(): exception("Index out of range !") {};
-//    const char * what () const throw () {
-//       return exception;
-//    }
-// };
-
-// template <class T>
-// class iterator: public std::iterator<std::random_access_iterator_tag, T>
-// {
-//         T* p;
-//     public:
-//         iterator(): p(0) {}
-//         iterator(T* x): p(x) {}
-//         iterator(const iterator& mit) : p(mit.p) {}
-//         iterator& operator++() {++p;return *this;}
-//         iterator& operator--() {--p;return *this;}
-//         bool operator==(const iterator& rhs) const {return p==rhs.p;}
-//         bool operator!=(const iterator& rhs) const {return p!=rhs.p;}
-//         int& operator*() {return *p;}
-// };
-
-// template <class Category, class T, class Distance = ptrdiff_t,
-//           class Pointer = T*, class Reference = T&>
-//   struct iterator {
-//     typedef T         value_type;
-//     typedef Distance  difference_type;
-//     typedef Pointer   pointer;
-//     typedef Reference reference;
-//     typedef Category  iterator_category;
-//   };
-
-template <class T>
+template < class T, class Alloc = std::allocator<T> >
 class Vector
 {
 private:
@@ -50,13 +15,13 @@ private:
     T* _begin;
     T* _end;
     T* _capacity;
-    std::allocator<T> alloc;
+    Alloc alloc;
 
 public:
     Vector();
     Vector( int n, T val );
     Vector( T* first, T* last );
-    Vector(const Vector& arr);
+    Vector( Vector& v );
     Vector&  operator=(const Vector& arr);
     T&      operator[] (int index) const ;
     ~Vector();
@@ -64,42 +29,64 @@ public:
     int size() const ;
     int capacity() const ;
 
-    class iterator
+    class iterator: std::iterator<std::random_access_iterator_tag, T>
     {
             T* p;
         public:
             iterator(): p(0) {}
             iterator(T* x): p(x) {}
-            iterator operator=(const iterator& rhs) const { iterator iter(rhs.p);
-                return iter;} 
-            // iterator(const iterator& mit) : p(mit.p) {}
+            iterator operator=(const iterator& rhs) {
+                if (*this == rhs)
+                    return *this;
+                this->p = rhs.p;
+                return *this;}
+            iterator(const iterator& iter) : p(iter.p) {}
             iterator& operator++() {++p;return *this;}
             iterator& operator--() {--p;return *this;}
+            iterator operator++( int ) { iterator old(*this); ++p;return old; }
+            iterator operator--( int ) { iterator old(*this); --p;return old; }
             bool operator==(const iterator& rhs) const {return p==rhs.p;}
             bool operator!=(const iterator& rhs) const {return p!=rhs.p;}
             T& operator*() {return *p;}
     };
 
-    iterator begin() { iterator iter(data);
-        return iter; };
-    iterator end() {return iterator(data + this->size()); } ;
+    class reverse_iterator
+    {
+            T* p;
+        public:
+            reverse_iterator(): p(0) {}
+            reverse_iterator(T* x): p(x) {}
+            reverse_iterator operator=(const reverse_iterator& rhs) {
+                if (*this == rhs)
+                    return *this;
+                this->p = rhs.p;
+                return *this;}
+            reverse_iterator(const reverse_iterator& iter) : p(iter.p) {}
+            reverse_iterator& operator++() {--p;return *this;}
+            reverse_iterator& operator--() {++p;return *this;}
+            reverse_iterator operator++( int ) { reverse_iterator old(*this); --p;return old; }
+            reverse_iterator operator--( int ) { reverse_iterator old(*this); ++p;return old; }
+            bool operator==(const reverse_iterator& rhs) const {return p==rhs.p;}
+            bool operator!=(const reverse_iterator& rhs) const {return p!=rhs.p;}
+            T& operator*() {return *p;}
+    };
 
-    // void  deepCopy(const Vector& arr);
-    // IndexOutOfRange _ioor;
+    iterator begin() { return iterator(data); };
+    iterator end() { return iterator(data + this->size()); };
+    reverse_iterator rbegin() { return reverse_iterator(data + this->size() - 1); };
+    reverse_iterator rend() { return reverse_iterator(data-1); };
     
 };
 
-template<class T>
-Vector<T>::Vector() {
-    std::cout << "Empty container constructor called" << std::endl;
+template<class T, class Alloc>
+Vector<T, Alloc>::Vector() {
     this->_begin = data;
     this->_end = data;
     this->_capacity = data;
 }
 
-template<class T>
-Vector<T>::Vector( int n, T val ) {
-    std::cout << "Fill constructor called" << std::endl;
+template<class T, class Alloc>
+Vector<T, Alloc>::Vector( int n, T val ) {
     this->data = alloc.allocate(n);
     for (int i = 0; i < n; i++)
         alloc.construct(data+i, val);
@@ -108,10 +95,9 @@ Vector<T>::Vector( int n, T val ) {
     this->_capacity = data + n;
 }
 
-template<class T>
-Vector<T>::Vector( T* first, T* last ) {
+template<class T, class Alloc>
+Vector<T, Alloc>::Vector( T* first, T* last ) {
     int size = last - first;
-    std::cout << "Range constructor called " << size << std::endl;
     this->data = alloc.allocate(size);
     int i = 0;
     while (first != last) {
@@ -124,67 +110,42 @@ Vector<T>::Vector( T* first, T* last ) {
     this->_capacity = data + size;
 }
 
-template<class T>
-Vector<T>::~Vector() {
-    std::cout << "Destructor called" << std::endl;
+template<class T, class Alloc>
+Vector<T, Alloc>::Vector( Vector& v ) {
+    int size = v.size();
+    Vector<T, Alloc>::iterator first = v.begin();
+    Vector<T, Alloc>::iterator last = v.end();
+    this->data = alloc.allocate(size);
+    int i = 0;
+    while (first != last) {
+        alloc.construct(data+i, *first);
+        i++;
+        first++;
+    }
+    this->_begin = data;
+    this->_end = data + size;
+    this->_capacity = data + size;
+}
+
+template<class T, class Alloc>
+Vector<T, Alloc>::~Vector() {
     if (this->capacity() > 0)
         alloc.deallocate(this->data, this->capacity());
 }
 
-template<class T>
-int  Vector<T>::size() const {
+template<class T, class Alloc>
+int  Vector<T, Alloc>::size() const {
     return this->_end - this->_begin;
 }
 
-template<class T>
-int  Vector<T>::capacity() const {
+template<class T, class Alloc>
+int  Vector<T, Alloc>::capacity() const {
     return this->_capacity - this->_begin;
 }
 
-// template<class T>
-// void Vector<T>::deepCopy(const Vector& arr) {
-//     if (this->size != 0)
-//         delete [] this->data;
-//     this->size = arr.size();
-//     if (arr.data != 0)
-//     {
-//         this->data = new T[this->size];
-//         for (int i = 0; i < this->size; i++)
-//         {
-//             this->data[i] = arr[i];
-//         }
-//     }
-//     else
-//         this->data = 0;
-// }
-
-// template<class T>
-// Vector<T>::Vector(const Vector& arr):
-// {
-//     deepCopy(arr);
-// }
-
-// template<class T>
-// Vector<T>&  Vector<T>::operator=(const Vector& arr) {
-//     deepCopy(arr);
-//     return *this;
-// }
-
-template<class T>
-T&      Vector<T>::operator[] (int index) const {
-    // if (index >= this->size()) {
-    //     throw _ioor;
-    // }
+template<class T, class Alloc>
+T&      Vector<T, Alloc>::operator[] (int index) const {
     return this->data[index];
 }
-
-// template<class T>
-// std::ostream&   operator<<(std::ostream& out, Vector<T>& arr) {
-//     int n = arr.size();
-//     for (int i = 0; i < n; i++) {
-//         out << arr[i] << std::endl;
-//     }
-//     return out;
-// }
 
 #endif
--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <vector>
 #include <iostream>
+#include <algorithm>
 #include "Vector.hpp"
 
 int main()
@@ -9,23 +10,27 @@ int main()
 
     int p [] = {16, 2, 77, 40, 12071};
     Vector<int>    v3(p, p+5);
+    Vector<int>    v4(v3);
 
-    Vector<int>::iterator f_iter(v3.begin());
-    Vector<int>::iterator l_iter(v3.end());
+    Vector<int>::iterator f_iter;
+    Vector<int>::iterator l_iter;
 
-    // f_iter = v3.begin();
-    // l_iter = v3.end();
+    f_iter = v3.begin();
+    l_iter = v3.end();
 
-        // problem in assignement op
+    Vector<int>::reverse_iterator rf_iter;
+    Vector<int>::reverse_iterator rl_iter;
+
+    rf_iter = v3.rbegin();
+    rl_iter = v3.rend();
 
     int i = 0;
-    while (f_iter != l_iter)
+    while (rf_iter != rl_iter)
     {
-        std::cout << "iterator " << *f_iter << std::endl;
-        std::cout << "index [] " << v3[i] << std::endl << std::endl;
-        ++f_iter;
+        std::cout << "iterator " << *rf_iter << std::endl;
+        ++rf_iter;
         i++;
     }
-    
+
     return 0;
 }
