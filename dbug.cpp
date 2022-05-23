4c65250ba9280b452ab7cfe0533053472424f611
Othmane Bounri <obounri@e1r7p13.1337.ma>
2022-05-23 20:59:32 +0100

    equal() and lex-compare()

--- dbug.cpp
+++ dbug.cpp
@@ -5,45 +5,26 @@
 #include "Vector.hpp"
 #include "Stack.hpp"
 #include "fcts.hpp"
+#include "pair.hpp"
 
-// int main()
-// {
+int main()
+{
 
-//     Vector<int> v(5, 5);
+    // Vector<int> v(5, 5);
 
-//     stack<int> s(v);
-//     s.push(8);
-//     std::cout << "is empty ? " << s.empty() << " size " << s.size() << " top " << s.top() << std::endl;
-//     for (size_t i = 0; i < 6; i++)
-//     {
-//         s.pop();
-//     }
-//     std::cout << "is empty ? " << s.empty() << " size " << s.size() << " top " << s.top() << std::endl;
+    // stack<int> s(v);
+    // s.push(8);
+    // std::cout << "is empty ? " << s.empty() << " size " << s.size() << " top " << s.top() << std::endl;
+    // for (size_t i = 0; i < 6; i++)
+    // {
+    //     s.pop();
+    // }
+    // std::cout << "is empty ? " << s.empty() << " size " << s.size() << " top " << s.top() << std::endl;
+  
+    pair<std::string, int> p; 
     
 
-//     std::cout << std::endl << "-------------------------------------------------------------------" << std::endl;
+    std::cout << std::endl << "-------------------------------------------------------------------" << std::endl;
     
-//     return 0;
-// }
-
-bool mycomp (char c1, char c2)
-{ return std::tolower(c1)<std::tolower(c2); }
-
-int main () {
-  char foo[]="Apple";
-  char bar[]="apartment";
-
-  std::cout << std::boolalpha;
-
-  std::cout << "Comparing foo and bar lexicographically (foo<bar):\n";
-
-  std::cout << "Using default comparison (operator<): ";
-  std::cout << lexicographical_compare(foo,foo+5,bar,bar+9);
-  std::cout << '\n';
-
-  std::cout << "Using mycomp as comparison object: ";
-  std::cout << lexicographical_compare(foo,foo+5,bar,bar+9,mycomp);
-  std::cout << '\n';
-
-  return 0;
+    return 0;
 }
---
+++ pair.hpp
@@ -0,0 +1,19 @@
+#ifndef PAIR_HPP
+#define PAIR_HPP
+
+template <class T1, class T2>
+struct pair 
+{
+    typedef T1  first_type;
+    typedef T2  second_type;
+
+    first_type  first;
+    second_type second;
+
+    pair():first(), second() {} ;
+    
+    template<class U, class V> pair(const pair<U,V>& pr):first(pr.first), second(pr.second) {} ;
+    pair(const first_type& a, const second_type& b): first(a), second(b) {} ;
+};
+
+#endif
